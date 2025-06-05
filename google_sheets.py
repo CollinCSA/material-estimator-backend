d46d7466ba6514fa3f285f7434ad3c7228be54b3
CollinCSA <collin.m@comsolaraz.com>
2025-06-05 15:28:26 -0700

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -13,9 +13,12 @@ def authorize():
     creds_dict = json.loads(os.environ['GOOGLE_CREDS'])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPES)
     client = gspread.authorize(creds)
+    
+    print([s.title for s in client.opena11()])
+    
     sheet = client.open(SHEET_NAME).worksheet(TAB_NAME)
     return sheet
-
+    
 def get_all_projects():
     sheet = authorize()
     return sheet.get_all_records()
